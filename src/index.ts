b9db4122f9d0578e1a87a700e1674b0b48916c6b
YanKutomanov <yan.kutomanov@leiainc.com>
2025-02-24 15:24:03 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,37 @@ import 'dotenv/config';
 
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
-const intervalList = new Map();
-const controllersMap = new Map();
+const chatsSet = new Set();
+const otomotoController = new OtomotoController();
 
 console.log('Bot is running');
+
+setInterval(() => checkWebsites(), 5 * 60 * 1000);
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   console.log('Started with chat ID: ', chatId);
   bot.sendMessage(chatId, 'Бот запущен! Начинаю мониторинг объявлений...');
-  if (!controllersMap.has(chatId)) {
-    controllersMap.set(chatId, [new OtomotoController()]);
-  }
-  checkWebsites(chatId);
-  intervalList.set(chatId, setInterval(() => checkWebsites(chatId), 5 * 60 * 1000));
+  chatsSet.add(chatId);
 });
 
 bot.onText(/\/stop/, (msg) => {
-  clearInterval(intervalList.get(msg.chat.id));
+  chatsSet.delete(msg.chat.id);
   console.log('Stopped with chat ID: ', msg.chat.id);
   bot.sendMessage(msg.chat.id, `Мониторинг для чата ${msg.chat.id} остановлен.`);
 });
 
-const checkWebsites = async (chatId: string) => {
-  console.log(`Checking websites for chat ${chatId} in ${new Date().toLocaleDateString()}`);
+const checkWebsites = async () => {
   try {
-    const controllersList = controllersMap.get(chatId);
-    const newItems = await Promise.all(controllersList.map(controller => controller.checkForNewListings()));
-
+    const newItems = await otomotoController.checkForNewListings();
+    console.log('Checked in: ', new Date().toLocaleString());
     const newItemsFlat = newItems.flat();
     if (newItemsFlat.length === 0) {
-      // bot.sendMessage(chatId, `Новых объявлений не найдено для чата ${chatId}`);
       return;
     } else {
-      bot.sendMessage(chatId, `Найдено ${newItemsFlat.length} новых объявлений: \n${newItemsFlat.join('\n')}`);
+      chatsSet.forEach(chatId => {
+        bot.sendMessage(chatId, `Найдено ${newItemsFlat.length} новых объявлений: \n${newItemsFlat.join('\n\n')}`);
+      });
     }
   } catch (error) {
     console.error(`❌ Error checking websites: ${error.message}`);
